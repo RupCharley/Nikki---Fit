6912296863731132b834f83f74809db00caaa899
RupCharley <rupcharley@gmail.com>
2026-03-01 23:31:55 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'nikki-v5';
+const FILES = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))  // delete ALL caches, not just old ones
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try network, fall back to cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});

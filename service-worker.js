6a1d324d9fa77d17f07301004ad24d816e27fe9a
RupCharley <rupcharley@gmail.com>
2026-03-02 15:42:24 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,18 @@
-const CACHE = 'nikki-v5';
-const FILES = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
+const CACHE = 'nikki-v6';
+const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))  // delete ALL caches, not just old ones
-    )
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-// Network-first: always try network, fall back to cache
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
